cdc37c80a8d376c5e1b6a3f1381645740be90cf8
Londot <bryan.londot@hotmail.com>
2019-12-18 16:42:39 +0100

    on a galerer pour le modal mais c Dieu qui donne merceeee

--- public/index.js
+++ public/index.js
@@ -1,3 +1,16 @@
-$('#blogCarousel').carousel({
-    interval: 5000
-});
+
+// modal
+
+let modalConect =document.querySelector('.modalConect')
+console.log(modalConect);
+let btnmodal = modalConect.querySelector('.btnzeub')
+console.log(btnmodal)
+let monmodal = document.querySelector('.modal')
+let spancloclo = document.querySelector('.close')
+
+btnmodal.addEventListener('click',() =>{
+    monmodal.style.display ="block"
+})
+spancloclo.addEventListener('click',()=>{
+    monmodal.style.display="none"
+})
