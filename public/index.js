6732b1172c3143a325c673c8a6e3438f4cc76cad
Londot <bryan.londot@hotmail.com>
2019-12-19 09:57:45 +0100

    fin du changement de thème black&white

--- public/index.js
+++ public/index.js
@@ -14,3 +14,29 @@ btnmodal.addEventListener('click',() =>{
 spancloclo.addEventListener('click',()=>{
     monmodal.style.display="none"
 })
+
+
+
+// changement de thème
+
+let changecolor =document.querySelector('.modalConect')
+let bouton1 = changecolor.querySelector('.themeWhite')
+let bouton2 = changecolor.querySelector('.themeNoir')
+let monBody = document.body
+let shopAt = document.querySelector('.shopAt')
+let para = shopAt.querySelector('p')
+
+
+bouton2.addEventListener('click',()=>{
+    monBody.style.backgroundColor= "grey"
+    bouton1.style.backgroundColor ="white"
+    monBody.style.color= "white"
+    para.style.color="black"
+    
+    
+})
+bouton1.addEventListener('click',()=>{
+    monBody.style.backgroundColor="white"
+    monBody.style.color=""
+    para.style.color="black"
+})
